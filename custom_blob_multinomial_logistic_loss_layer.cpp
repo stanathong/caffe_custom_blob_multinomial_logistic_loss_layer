7cc2eed4bc7e3d7eeabf2e72137bf017540d2967
Supannee Tanathong <stanathong@users.noreply.github.com>
2018-03-01 22:59:47 +0000

    First commit

---
+++ custom_blob_multinomial_logistic_loss_layer.cpp
@@ -0,0 +1,218 @@
+#include <algorithm>
+#include <cfloat> // Add following SoftmaxWithLoss
+#include <cmath>
+#include <vector>
+
+
+#include "caffe/layer.hpp"	// Add following SoftmaxWithLoss
+#include "caffe/layers/custom_blob_multinomial_logistic_loss_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void CustomBlobMultinomialLogisticLossLayer<Dtype>::LayerSetUp(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) 
+{
+  LossLayer<Dtype>::LayerSetUp(bottom, top);
+
+  // Validate the shape of prob blob and label blob
+  CHECK_EQ(bottom[0]->num(), bottom[1]->num());
+  CHECK_EQ(bottom[1]->channels(), 1);
+  CHECK_EQ(bottom[0]->height(), bottom[1]->height());
+  CHECK_EQ(bottom[0]->width(), bottom[1]->width());
+
+  has_ignore_label_ =
+    this->layer_param_.loss_param().has_ignore_label();
+  if (has_ignore_label_) 
+  {
+    ignore_label_ = this->layer_param_.loss_param().ignore_label();
+  }
+  normalize_ = this->layer_param_.loss_param().normalize();
+  weight_by_label_freqs_ =
+    this->layer_param_.loss_param().weight_by_label_freqs();
+  
+  if (weight_by_label_freqs_) 
+  {
+    vector<int> count_shape(1, this->layer_param_.loss_param().class_weighting_size());
+    label_counts_.Reshape(count_shape);
+    CHECK_EQ(this->layer_param_.loss_param().class_weighting_size(), bottom[0]->channels())
+		<< "Number of class weight values does not match the number of classes.";
+    float* label_count_data = label_counts_.mutable_cpu_data();
+    for (int i = 0; i < this->layer_param_.loss_param().class_weighting_size(); i++) 
+	{
+        label_count_data[i] = this->layer_param_.loss_param().class_weighting(i);
+    }
+  }
+}
+
+template <typename Dtype>
+void CustomBlobMultinomialLogisticLossLayer<Dtype>::Reshape(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) 
+{
+  LossLayer<Dtype>::Reshape(bottom, top);
+
+  softmax_axis_ = 
+      bottom[0]->CanonicalAxisIndex(this->layer_param_.softmax_param().axis());
+
+  CHECK_EQ(softmax_axis_, 1) << "The probability axis must be the same as channel axis." ;
+
+  outer_num_ = bottom[0]->count(0, softmax_axis_);
+  inner_num_ = bottom[0]->count(softmax_axis_ + 1);
+
+  if (top.size() >= 2) 
+  {
+    top[1]->ReshapeLike(*bottom[0]);
+  }
+  if (weight_by_label_freqs_) 
+  {
+    CHECK_EQ(this->layer_param_.loss_param().class_weighting_size(), bottom[0]->channels())
+		<< "Number of class weight values does not match the number of classes.";
+  }
+}
+
+template <typename Dtype>
+void CustomBlobMultinomialLogisticLossLayer<Dtype>::Forward_cpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+{
+  const Dtype* prob_data = bottom[0]->cpu_data();
+  const Dtype* label = bottom[1]->cpu_data();
+  //int num = bottom[0]->num();
+  int nlabel = bottom[0]->shape(softmax_axis_);
+  int dim = bottom[0]->count() / outer_num_;
+  int count = 0;
+  Dtype loss = 0;
+//  for (int i = 0; i < num; ++i) {
+//    int label = static_cast<int>(bottom_label[i]);
+//    Dtype prob = std::max(
+//        bottom_data[i * dim + label], Dtype(kLOG_THRESHOLD));
+//    loss -= log(prob);
+//  }
+//  top[0]->mutable_cpu_data()[0] = loss / num;
+
+  for (int i = 0; i < outer_num_; ++i)	// N
+  {
+    for (int j = 0; j < inner_num_; j++) // HxW
+	{
+      const int label_value = static_cast<int>(label[i * inner_num_ + j]);
+      if (has_ignore_label_ && label_value == ignore_label_) 
+	  {
+        continue;
+      }
+      DCHECK_GE(label_value, 0);
+      DCHECK_LT(label_value, nlabel);
+      const int idx = i * dim + label_value * inner_num_ + j;
+      if (weight_by_label_freqs_) 
+	  {
+        const float* label_count_data = label_counts_.cpu_data();
+        loss -= log(std::max(prob_data[idx], Dtype(FLT_MIN)))
+            * static_cast<Dtype>(label_count_data[label_value]);
+      } else 
+	  {
+        loss -= log(std::max(prob_data[idx], Dtype(FLT_MIN)));
+      }
+      ++count;
+    }
+  }
+  if (normalize_) {
+    top[0]->mutable_cpu_data()[0] = loss / count;
+  } else {
+    top[0]->mutable_cpu_data()[0] = loss / outer_num_;
+  }
+  if (top.size() == 2) {
+    top[1]->ShareData(prob_);
+  }
+}
+
+template <typename Dtype>
+void CustomBlobMultinomialLogisticLossLayer<Dtype>::Backward_cpu(
+    const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down,
+    const vector<Blob<Dtype>*>& bottom) 
+{
+/*
+  if (propagate_down[1]) 
+  {
+    LOG(FATAL) << this->type()
+               << " Layer cannot backpropagate to label inputs.";
+  }
+  if (propagate_down[0]) 
+  {
+    const Dtype* bottom_data = bottom[0]->cpu_data();
+    const Dtype* bottom_label = bottom[1]->cpu_data();
+    Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+    int num = bottom[0]->num();
+    int dim = bottom[0]->count() / bottom[0]->num();
+    caffe_set(bottom[0]->count(), Dtype(0), bottom_diff);
+    const Dtype scale = - top[0]->cpu_diff()[0] / num;
+    for (int i = 0; i < num; ++i) {
+      int label = static_cast<int>(bottom_label[i]);
+      Dtype prob = std::max(
+          bottom_data[i * dim + label], Dtype(kLOG_THRESHOLD));
+      bottom_diff[i * dim + label] = scale / prob;
+    }
+  }
+*/
+  if (propagate_down[1]) 
+  {
+    LOG(FATAL) << this->type()
+               << " Layer cannot backpropagate to label inputs.";
+  }
+  if (propagate_down[0]) 
+  {
+    Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+    const Dtype* prob_data = bottom[0]->cpu_data();
+    caffe_copy(bottom[0]->count(), prob_data, bottom_diff);	
+			// copy probabilities to bottom_diff for NxCxHxW
+			// bottom_diff = S
+    const Dtype* label = bottom[1]->cpu_data();
+    int dim = bottom[0]->count() / outer_num_; // NxHxW
+    int count = 0;
+    const float* label_count_data = 
+        weight_by_label_freqs_ ? label_counts_.cpu_data() : NULL;
+    for (int i = 0; i < outer_num_; ++i) // N
+	{
+      for (int j = 0; j < inner_num_; ++j) // HxW
+	  {
+        const int label_value = static_cast<int>(label[i * inner_num_ + j]);
+        if (has_ignore_label_ && label_value == ignore_label_) 
+		{
+          for (int c = 0; c < bottom[0]->shape(softmax_axis_); ++c) 
+		  {
+            bottom_diff[i * dim + c * inner_num_ + j] = 0;
+          }
+        } 
+		else 
+		{
+          const int idx = i * dim + label_value * inner_num_ + j;
+		  // diff for correct label = S-1
+		  bottom_diff[idx] -= 1;
+
+          if (weight_by_label_freqs_) 
+		  {
+            for (int c = 0; c < bottom[0]->shape(softmax_axis_); ++c) 
+			{
+              bottom_diff[i * dim + c * inner_num_ + j] *= static_cast<Dtype>(label_count_data[label_value]);
+            }
+          }
+          ++count;
+        }
+      }
+    }
+    // Scale gradient
+    const Dtype loss_weight = top[0]->cpu_diff()[0];
+    if (normalize_) {
+      caffe_scal(prob_.count(), loss_weight / count, bottom_diff);
+    } else {
+      caffe_scal(prob_.count(), loss_weight / outer_num_, bottom_diff);
+    }
+  }
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(CustomBlobMultinomialLogisticLossLayer);
+#endif
+
+INSTANTIATE_CLASS(CustomBlobMultinomialLogisticLossLayer);
+REGISTER_LAYER_CLASS(CustomBlobMultinomialLogisticLoss);
+
+}  // namespace caffe
---
+++ custom_blob_multinomial_logistic_loss_layer.hpp
@@ -0,0 +1,106 @@
+#ifndef CAFFE_CUSTOM_BLOB_MULTINOMIAL_LOGISTIC_LOSS_LAYER_HPP_
+#define CAFFE_CUSTOM_BLOB_MULTINOMIAL_LOGISTIC_LOSS_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+#include "caffe/layers/loss_layer.hpp"
+
+namespace caffe {
+
+/* 
+ * Description: Computes loss based on multinomial logistic loss
+ *		The code is adapted from multinomial_logistic_loss_layer and
+ *		softmax_loss_layer.
+ * Input: bottom input Blob vector (length 2)	
+ *	1) the probabilities from softmax
+ *			N x #grade x H x W
+ *	2) label of size N x 1 x H x W
+ * Output: top output Blob vector (length 1)
+ *	loss: 1x1x1x1
+ */
+template <typename Dtype>
+class CustomBlobMultinomialLogisticLossLayer : public LossLayer<Dtype> {
+ public:
+  explicit CustomBlobMultinomialLogisticLossLayer(const LayerParameter& param)
+      : LossLayer<Dtype>(param) {}
+  // Added LayerSetUp as it exists in SoftmaxWithLossLayer but not in MultinomialLogLoss
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "CustomBlobMultinomialLogisticLoss"; }
+
+  // Added following SoftmaxWithLossLayer
+  virtual inline int ExactNumTopBlobs() const { return -1; }
+  virtual inline int MinTopBlobs() const { return 1; }
+  virtual inline int MaxTopBlobs() const { return 2; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  // Added following SoftmaxWithLossLayer
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  /**
+   * @brief Computes the multinomial logistic loss error gradient w.r.t. the
+   *        predictions.
+   *
+   * Gradients cannot be computed with respect to the label inputs (bottom[1]),
+   * so this method ignores bottom[1] and requires !propagate_down[1], crashing
+   * if propagate_down[1] is set.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
+   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
+   *      as @f$ \lambda @f$ is the coefficient of this layer's output
+   *      @f$\ell_i@f$ in the overall Net loss
+   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
+   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
+   *      (*Assuming that this top Blob is not used as a bottom (input) by any
+   *      other layer of the Net.)
+   * @param propagate_down see Layer::Backward.
+   *      propagate_down[1] must be false as we can't compute gradients with
+   *      respect to the labels.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the predictions @f$ \hat{p} @f$; Backward computes diff
+   *      @f$ \frac{\partial E}{\partial \hat{p}} @f$
+   *   -# @f$ (N \times 1 \times H \times W) @f$ // modify label to be of size Nx1xHxC 
+   *	  instead of Nx1x1x1
+   *      the labels -- ignored as we can't compute their error gradients
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  // Added following SoftmaxWithLossLayer
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  // Add the member variables according to SoftmaxWithLossLayer
+
+  /// prob stores the output probability predictions from the SoftmaxLayer.
+  Blob<Dtype> prob_;
+  /// Whether to ignore instances with a certain label.
+  bool has_ignore_label_;
+  /// The label indicating that an instance should be ignored.
+  int ignore_label_;
+  /// Whether to normalize the loss by the total number of values present
+  /// (otherwise just by the batch size).
+  bool normalize_;
+  /// Whether to weight labels by their batch frequencies when calculating
+  /// the loss
+  bool weight_by_label_freqs_;
+  Blob<float> label_counts_; // For Blob data, before use, must get data from memory either const or mutable.
+
+  int softmax_axis_, outer_num_, inner_num_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_CUSTOM_BLOB_MULTINOMIAL_LOGISTIC_LOSS_LAYER_HPP_
